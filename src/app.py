1d0e3cac8d411f084f37689c70bb470314c01798
Martin Merkli <martin.merkli@protonmail.com>
2023-02-18 19:30:03 +0100

    Basic Setup
    Basic flask setup.

--- src/app.py
+++ src/app.py
@@ -1,7 +1,84 @@
+#!/usr/bin/env python3
+
+########################################################################################################################
+# IMPORTS
+########################################################################################################################
+
+
 from flask import Flask
+from logging import FileHandler as LogFileHandler, StreamHandler as LogStreamHandler, log as logging_log
+from logging import basicConfig as log_basicConfig, getLogger as GetLogger, Formatter as LogFormatter
+from logging import ERROR as LOG_ERROR, INFO as LOG_INFO
+from os import urandom
+from os.path import exists, join
+from sqlite3 import connect as sqlite_connect
+
+########################################################################################################################
+# GENERAL SETUP
+########################################################################################################################
+
 
 app = Flask(__name__)
 
+if not exists(join(app.root_path, 'resources', 'key.bin')):
+    with open(join(app.root_path, 'resources', 'key.bin'), 'wb') as _f:
+        _f.write(urandom(64))
+with open(join(app.root_path, 'resources', 'key.bin'), 'rb') as _f:
+    _secret_key = _f.read()
+app.secret_key = _secret_key
+
+
+########################################################################################################################
+# LOGGING
+########################################################################################################################
+
+
+def setup_logger(name, file):
+    logger = GetLogger(name)
+    formatter = LogFormatter('%(asctime)s\t%(message)s', datefmt='%Y-%m-%d_%H-%M-%S')
+    file_handler = LogFileHandler(file, mode='a')
+    file_handler.setFormatter(formatter)
+    stream_handler = LogStreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.setLevel(LOG_INFO)
+    logger.addHandler(file_handler)
+    logger.addHandler(stream_handler)
+
+
+log_basicConfig(filename='main.log', format='%(asctime)s\t%(message)s', datefmt='%Y-%m-%d_%H-%M-%S', level=LOG_INFO)
+
+setup_logger('access', join(app.root_path, 'logs', 'access.log'))
+access_log = GetLogger('access')
+
+setup_logger('abuse_report', join(app.root_path, 'logs', 'abuse_report.log'))
+abuse_report_log = GetLogger('abuse_report')
+
+setup_logger('request_errors', join(app.root_path, 'logs', 'request_errors.log'))
+request_errors_log = GetLogger('request_errors')
+
+
+########################################################################################################################
+# DATABASE SETUP
+########################################################################################################################
+
+
+conn = sqlite_connect('database.db')
+db = conn.cursor()
+
+
+########################################################################################################################
+# MAIN
+########################################################################################################################
+
 
 if __name__ == '__main__':
-    app.run()
+    try:
+        app.run('0.0.0.0', 8000)
+    except Exception as e:
+        print(e)
+        logging_log(LOG_ERROR, e)
+    try:
+        conn.close()
+    except Exception as e:
+        print(e)
+        logging_log(LOG_ERROR, e)
